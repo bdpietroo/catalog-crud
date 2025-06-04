229df245afbded8d5cb5bd9f8cbdbd2e1347e920
bdpietroo <brunopietro167@gmail.com>
2025-06-04 16:18:05 -0300

    entity created

---
+++ src/main/java/com/bdpietroo/product_catolog_api/entities/Produto.java
@@ -0,0 +1,28 @@
+package com.bdpietroo.product_catolog_api.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private Integer quantidadeEstoque;
+}
