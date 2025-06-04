778a2938d3bb6b295b5cc767a38abbb625015774
bdpietroo <brunopietro167@gmail.com>
2025-06-04 15:35:35 -0300

    creating project

---
+++ src/main/java/com/bdpietroo/product_catolog_api/ProductCatologApiApplication.java
@@ -0,0 +1,13 @@
+package com.bdpietroo.product_catolog_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatologApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatologApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdpietroo/product_catolog_api/ProductCatologApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdpietroo.product_catolog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatologApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
